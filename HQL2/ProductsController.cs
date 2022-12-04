051355933b570073d452b10b897a855203c1bd45
Deepika625 <118673564+Deepika625@users.noreply.github.com>
2022-12-04 06:11:53 +0000

    HOL2

---
+++ HQL2/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_HOL2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            Models.Product p = new Models.Product();
+            return View(p.GetProducts());
+
+
+
+
+
+        }
+    }
+}
---
+++ HQL2/products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5_HOL2.Models
+{
+    public class products
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Prince { get; set; }
+        public IEnumerable<products> GetProducts()
+        {
+            return new List<products>()
+            {
+            new products ProductId=1, ProductName = "Product1", Price = 900.00 },
+            new products { ProductId = 2, ProductName = "Product2" Price = 700.00},
+            new products { ProductId = 3, ProductName ="roduct3, Price = 650.00 },
+                new products { ProductId = 4, ProductName = "Product4, Price = 950.00 },
+                new products { ProductId = 5, ProductName = "Product5, Price = 800.00 },
+
+
+                };
+            }
+             
+    }
+}
