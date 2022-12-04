e9c0b66655d289ebdd5af61c5196861962a0c75d
Deepika625 <118673564+Deepika625@users.noreply.github.com>
2022-12-04 19:57:08 +0530

    HOL6

---
+++ HOL6/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using HOL6.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace HOL6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult GetServerTime()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return PartialView();
+        }
+        static List<user> users = new List<user>();
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateUser(user u )
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ HOL6/user.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HOL6.Models
+{
+    public class user
+        
+
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
